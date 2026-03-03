25fbfe0b7641cfc55e75e168f4855095bcb24550
HypeCloud Macbook <apple@Mac.lan>
2026-03-03 20:25:49 +0700

    feat: update index.js with filtering logic

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-const provinceName = ["Ha Noi", "Hai Phong", "Da Nang", "Ho Chi Minh City", "Can Tho"];
+// const Provinces = ["Hà Nội", "Hồ Chí Minh", "Đà Nẵng", "Hải Phòng", "Cần Thơ", "An Giang"];
 
-provinceName.forEach((province, index) => {
-    console.log(province);
-});
+// Provinces.forEach((province) => {
+//     console.log(province);
+// });
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const filteredNumbers = numbers.filter((number) => number % 2 === 0);
+console.log(filteredNumbers); 
 
